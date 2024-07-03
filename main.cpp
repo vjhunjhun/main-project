d0287caca0d24753115dad6c04d4aa3e39676d75
vjhunjhun <jhunjhunwalavaibhav20@gmail.com>
2024-07-03 08:06:37 +0545

    final1

--- main.cpp
+++ main.cpp
@@ -9,28 +9,40 @@ bool IsPointInsideRectangle(Vector2 point, Rectangle rect) {
     return (point.x >= rect.x && point.x <= rect.x + rect.width &&
             point.y >= rect.y && point.y <= rect.y + rect.height);
 }
-
+bool IsAnyKeyPressed() {
+    for (int key = 32; key < 349; key++) { // 32 to 348 are the key codes in raylib
+        if (IsKeyPressed(key)) {
+            return true;
+        }
+    }
+    return false;
+}
+ 
 int main() {
     // Initialization
     const int screenWidth = 800;
     const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Simons memory game");
+    InitAudioDevice();
+     Sound beepSound = LoadSound("resources/wrongAnswerSound.wav");
+     
     vector<int> gameSeq;
     // vector<int> playerSeq;
     int *playerSeq = new int[0];
-    Texture2D background = LoadTexture("resources/background.png");
+    Texture2D background = LoadTexture("resources/background1.png");
+    Texture2D startBg = LoadTexture("resources/startBg.png");
     Font font = LoadFontEx("resources/JokermanFont.ttf", 150,0,250);
-    Font font1 = LoadFontEx("resources/BlackadderITC.otf", 150,0,250);
+    Font font1 = LoadFontEx("resources/PressStart2p.ttf", 150,0,250);
     // if(font.baseSize>0){
     //     cout<<"pic not found ";
     //     return -1;
     // }
     // Box variables
     Rectangle boxes[4] = {
-        {150, 100, 150, 100},
-        {450, 100, 150, 100},
-        {150, 300, 150, 100}, 
-        {450, 300, 150, 100}
+        {225, 175, 150, 100},
+        {425, 175, 150, 100},
+        {225, 325, 150, 100}, 
+        {425, 325, 150, 100}
     };
     
     Color boxColors[4] = {RED, GREEN, BLUE, YELLOW};
@@ -43,21 +55,34 @@ int main() {
 
     SetTargetFPS(60); // Set the frame rate
       int level =0; 
-      bool out = false;
-      int *answer = new int[100];
-      int *userInput = new int[level];
-      int flag = 0;
-      float startTimer=0.500f;
-      bool startCond=true;
-      int prevRandom = -1;
-      bool check=true;
-    
-      //bool out1=true;
+      int highscore=0;
+      bool out = true;
+      bool play = false;
+      
+    //   float startTime=2.50f;
     // Main game loop
     while (!WindowShouldClose()) { // Detect window close button or ESC key
-      
+   bool keySpacePressed = IsKeyPressed(KEY_SPACE);
+      while(!play){
+          BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawTexture(startBg, 0, 0, WHITE);
+        if(WindowShouldClose()){
+                CloseWindow(); // Close window and OpenGL context
+                 return 0;
+            } 
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || IsAnyKeyPressed()){
+             out=false;
+            play=true;
+//          UnloadTexture(startBg);
+           }
+           EndDrawing();
+      }
          while(!out){
             level++;
+            if(level>highscore){
+                highscore=level;
+            }
             playerSeq= new int[level];
             for(int i=0;i<level;i++){
                 playerSeq[i]=-1;
@@ -66,65 +91,26 @@ int main() {
                 CloseWindow(); // Close window and OpenGL context
                  return 0;
             } 
-            int randomNumber;
-            flag=0;
-              while(flag == 0){
-                 randomNumber = (rand() % 4);
-                 flag =1;
-            }
+            int randomNumber = (rand() % 4);
             gameSeq.push_back(randomNumber);
             for(int j=0;j<gameSeq.size();j++){
                 cout<<gameSeq[j];
             }
-            // DrawText(TextFormat("number: %d", randomNumber), 100, 50, 20, BLACK);
-            //  DrawText(TextFormat("Level: %d", level), 10, 100, 20, BLACK);
+            cout<<" ";
              blinkTimers[randomNumber]=0.450f;
-            // Vector2 mousePoint = GetMousePosition();
-             //float deltaTime = GetFrameTime(); // Get time elapsed between frames
-        //         for (int i = 0; i < 4; i++) {
-        //     if (clickTimers[i] > 0) {
-        //         clickTimers[i] -= deltaTime; // Decrease timer
-        //     }
-        //     if (IsPointInsideRectangle(mousePoint, boxes[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-        //         clickTimers[i] = 0.250f; // Set timer to 0.250 second when clicked    
-        //     }
-        // }
                BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawTexture(background, 0, 0, WHITE);
-        DrawText(TextFormat("number: %d", randomNumber), 100, 50, 20, BLACK);
-             DrawText(TextFormat("Level: %d", level), 10, 100, 20, BLACK);
+        //DrawText(TextFormat("number: %d", randomNumber), 100, 50, 20, BLACK);
+            DrawTextEx(font1, TextFormat("Level: %d", level), (Vector2){ screenWidth/80, screenHeight/7 }, 18, 1, DARKGRAY);
     for (int i = 0; i < 4; i++) {
-            // if (clickTimers[i] > 0) { 
-            //    if(clickTimers[0]>0) {out=true;}
-            //  DrawRectangleRec(boxes[i], boxClickColors);
-            // } else
-            //  if (IsPointInsideRectangle(mousePoint, boxes[i])) {
-            //     DrawRectangleRec(boxes[i], boxHoverColors[i]);
-            // } 
-            //else {
                 DrawRectangleRounded((Rectangle)boxes[i], 0.5f, 10, boxColors[i]);
-                // DrawRectangleRec(boxes[i], boxColors[i]);
-            //
+             
         }
         EndDrawing();
-        // while(startCond){
-        //     float deltaTime2=GetFrameTime();
-        //     if(startTimer>0){
-        //         startTimer-=deltaTime2;
-        //      DrawText("Game starting", 200, 50, 40, RED);
-        //      }
-        //    if(startTimer<=0.0f){
-        //       out1 = false;
-        //    }
-           
-        // }
        bool out1=false;
-         check=true;
-         int j=0;
+         int j=0,k=0;
         while(!out1){
-            startCond=false;
-            
             // check =true;
              Vector2 mousePoint = GetMousePosition();
              if(WindowShouldClose()){
@@ -148,19 +134,19 @@ int main() {
                      BeginDrawing();
         ClearBackground(RAYWHITE);
        DrawTexture(background, 0, 0, WHITE);
-        DrawText(TextFormat("number: %d", randomNumber), 100, 50, 20, BLACK);
-             DrawText(TextFormat("Level: %d", level), 10, 100, 20, BLACK);
+        //DrawText(TextFormat("number: %d", randomNumber), 100, 50, 20, BLACK);
+            DrawTextEx(font1, TextFormat("Level: %d", level), (Vector2){ screenWidth/80, screenHeight/7 }, 18, 1, DARKGRAY);
+             DrawTextEx(font1, TextFormat("Highest Score: %d", highscore), (Vector2){ screenWidth/2, screenHeight/7 }, 18, 1, DARKGRAY);
+             //DrawText(TextFormat("Level: %d", level), 10, 100, 20, BLACK);
 
     for (int i = 0; i < 4; i++) {
        //we can add timer here maybe
            
             if (blinkTimers[i] > 0) { 
                 DrawRectangleRounded((Rectangle)boxes[i], 0.5f, 10, boxblinkColors);
-             //DrawRectangleRec(boxes[i], boxblinkColors);
             }else if (clickTimers[i] > 0) { 
                 playerSeq[j++]=i;
-                int k=0;
-                //if(clickTimers[randomNumber]>0){
+                
                     bool mainloop=true; 
                     while(mainloop){
                     if(gameSeq[gameSeq.size()-1]==playerSeq[level-1]){
@@ -168,32 +154,24 @@ int main() {
                         out1=true;
                     }
                     else if(gameSeq[k]==playerSeq[k]){
-                        
-                k++;
+                       mainloop=false;  
+                    k++;
                     }else{
+                        PlaySound(beepSound);
                         mainloop=false;
                         out1=true;
                         out=true;
                     }
                 }
                 clickTimers[i]=0.0f;
-                //out1=true;
-              // }
-            // }else if (clickTimers[randomNumber]==0){
-            //     out1=true;
-            //     out=true;
-            // }
-            // else{
-            //     out1=true;
-            //     out=true;
-            // }
             DrawRectangleRounded((Rectangle)boxes[i], 0.5f, 10, boxClickColors);
              //DrawRectangleRec(boxes[i], boxClickColors);
             }else if (IsPointInsideRectangle(mousePoint, boxes[i])) {
                 DrawRectangleRounded((Rectangle)boxes[i], 0.5f, 10, boxHoverColors[i]);
-                // DrawRectangleRec(boxes[i], boxHoverColors[i]);
+               // DrawRectangleRec(boxes[i], boxHoverColors[i]);
             } else {
                 DrawRectangleRounded((Rectangle)boxes[i], 0.5f, 10, boxColors[i]);
+                
                 //DrawRectangleRec(boxes[i], boxColors[i]);
             }
             
@@ -201,15 +179,24 @@ int main() {
     // DrawText("Waiting", 300, 200, 40, RED);
         EndDrawing();
         }
-        prevRandom = randomNumber;
+        //prevRandom = randomNumber;
         }
            BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawTexture(background, 0, 0, WHITE);
-        DrawTextEx(font1, TextFormat("Game Over"), (Vector2){ screenWidth/7.5, screenHeight/2.5 }, 150, 1, BLACK);
+        DrawTextEx(font1, TextFormat("Game Over"), (Vector2){ screenWidth/8.5, screenHeight/2.5 }, 75, 1, BLACK);
         //DrawText("Game Over", screenWidth/5.5, screenHeight/3, 100, RED);
+         DrawTextEx(font1, TextFormat("press Esc To exit the game,\n\n space to play again.."), (Vector2){ screenWidth/9, screenHeight/1.5 }, 25, 1, BLACK);
+         if(keySpacePressed){
+         level =0; 
+       play = false;
+       gameSeq.clear();
+           }
         EndDrawing();
     }
+    UnloadSound(beepSound); // Unload sound
+    CloseAudioDevice();
+        UnloadTexture(startBg);
         UnloadTexture(background);
         CloseWindow(); // Close window and OpenGL context
      return 0;
